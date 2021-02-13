437d5450b47819b94a034991c1e98e2e2a8986c8
ChrisVIN321 <69074192+ChrisVIN321@users.noreply.github.com>
2021-02-13 15:21:33 +0200

    Add files via upload

--- hqes.py
+++ hqes.py
@@ -1,10 +1,10 @@
-# HIPPO QUICK EMAIL SERVICE v2.2 | Copyright 2021 Chris Vintsanis
+# HIPPO QUICK EMAIL SERVICE v2.3 | Copyright 2021 Chris Vintsanis
 
 import colorama
 from colorama import Fore, Style
 colorama.init(autoreset=True)
 
-print(Fore.BLUE + Style.BRIGHT + "Welcome to Hippo Quick Email Service v2.1!")
+print(Fore.BLUE + Style.BRIGHT + "Welcome to Hippo Quick Email Service v2.3!")
 print(Fore.CYAN + "1. Send an email")
 print(Fore.CYAN + "2. Read email")
 print(Fore.CYAN + "3. Exit the script\n")
@@ -12,10 +12,13 @@ print(Fore.CYAN + "3. Exit the script\n")
 while True:
 	action = str(input(''))
 	if action == '1':
-		import send_email
+		import send_email as s
+		s.send_email()
 	elif action == '2':
-		import read_email
+		import read_email as r
+		r.read_email()
 	elif action == '3':
+		print(Fore.RED + Style.BRIGHT + "Goodbye, :)")
 		exit()
 	else:
 		print(Fore.RED + "[ERROR]: Invalid option!")
--- readme.txt
+++ readme.txt
@@ -1,12 +1,22 @@
 Hippo Quick Email Service (HQES)
-v2.2
+v2.3
 Copyright 2021 Chris Vintsanis
 
 READ THIS BEFORE USING HQES!
 
-This only works with a Gmail account. To be able to use this, you have to enable
-less secure apps on your Google account.
+This only works with a Gmail account. Also, you need to enable access to less
+secure app on your Google account
 
-The script is not perfect, especially the reading part, but check out for upcoming updates.
+The program is not perfect, but stay tuned for upcoming releases
 
-If you find any bugs or if you have any questions, please contact me!
+If you find any bugs or have a question, please contact me!*
+
+-v2.3 CHANGE LOG
+
+This release mostly fixes some bugs!
+
+-TODO
+I plan to add a save system for your credentials so you don't have to type
+them every time
+
+*if for some reason you want an older version, you can always contact me too
